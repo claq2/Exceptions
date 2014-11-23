2b4a99f88bfacf8ac6f8a1f0c64833caaf06f6c3
James McLachlan <claquer@gmail.com>
2014-11-22 22:33:16 -0500

    More demo exceptions

--- Console/Program.cs
+++ Console/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Console
@@ -9,6 +10,34 @@ namespace Console
     class Program
     {
         static void Main(string[] args)
+        {
+            System.Console.WriteLine("1. Throw on this thread");
+            System.Console.WriteLine("2. Throw on new Thread thread");
+            System.Console.WriteLine("3. Throw on async thread");
+            int input = System.Console.Read();
+            char inputChar = (char)input;
+            if (inputChar == '1')
+            {
+                throw new Exception("Oh noooo!");
+            }
+            else if (inputChar == '2')
+            {
+                var thread = new Thread(ThreadProcedure);
+                thread.Start();
+            }
+            else if (inputChar == '3')
+            { 
+                AsyncProcedure().Wait();
+            }
+            
+        }
+
+        static async Task AsyncProcedure()
+        {
+            throw new Exception("Oh noooo!");
+        }
+
+        private static void ThreadProcedure(object state)
         {
             throw new Exception("Oh noooo!");
         }
